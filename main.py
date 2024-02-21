8c6406dc4302e3f06961e889a60ebcf126872e28
AnjiPythonUser <andreyavtogruz@gmail.com>
2024-02-21 08:05:38 +0300

    Начальный коммит

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/sum")
+def read_item(value1: int, value2: int):
+    return {
+        "value1": value1,
+        "value2": value2,
+        "result": value1 + value2
+    }
